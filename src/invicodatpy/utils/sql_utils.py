6ea17d8df3187750dbc8c9179c29d4c8955638fe
Fernando Corrales <corrales_fernando@hotmail.com>
2022-12-16 14:49:22 -0300

    feat: add methods to sql_utils.py

--- src/invicodatpy/utils/sql_utils.py
+++ src/invicodatpy/utils/sql_utils.py
@@ -3,11 +3,17 @@
 Author: Fernando Corrales <fscorrales@gmail.com>
 Purpose: SQL methods
 """
-from pandas import DataFrame
-from sqlalchemy import MetaData, Table, and_, delete, engine
+import pandas as pd
+from sqlalchemy import MetaData, Table, and_, create_engine, delete, engine
 
 
 class SQLUtils():
+    "Some generals methods"
+    df = None
+    _TABLE_NAME = ''
+    _INDEX_COL = ''
+    _FILTER_COL = ''
+    _SQL_MODEL = None
     
     # --------------------------------------------------
     """Delete rows from a table with one or multiple conditions
@@ -17,41 +23,78 @@ class SQLUtils():
     :param col_filter: column (str) or multiple columns (str) to
     look for unique values
     """
-    def delete_rows_with_df_col(
-        self, engine:engine, df:DataFrame,
-        table_filter:str, col_filter:str | list
-    ):
-        connection = engine.connect()
+    def delete_rows_with_df_col(self):
+        connection = self.engine.connect()
         metadata = MetaData()
-        sql_table = Table(table_filter, 
-        metadata, autoload=True, autoload_with=engine)
+        sql_table = Table(self._TABLE_NAME, 
+        metadata, autoload=True, autoload_with=self.engine)
         
-        if isinstance(col_filter, list):
+        if isinstance(self._FILTER_COL, list):
             where_lst = []
-            for i in col_filter:
-                unique_col = df[i].unique()
+            for i in self._FILTER_COL:
+                unique_col = self.df[i].unique()
                 where_lst.append(sql_table.c[i].in_(unique_col))
             where_clause = and_(*where_lst)
         else:
-            unique_col = df[col_filter].unique()
-            where_clause = sql_table.c[col_filter].in_(unique_col)
+            unique_col = self.df[self._FILTER_COL].unique()
+            where_clause = sql_table.c[self._FILTER_COL].in_(unique_col)
         
         u = delete(sql_table).where(where_clause)
         result = connection.execute(u)
         return result
 
     # --------------------------------------------------
-    """Delete all rows from a table
-    :param engine: sqlalchemy engine to connect to
-    :param table_filter: Table name from where to delete rows
-    """
-    def delete_all_rows(
-        self, engine:engine, table_filter:str
-    ):
+    def delete_all_rows(self):
+        """Delete all rows from a table
+        :param engine: sqlalchemy engine to connect to
+        :param table_filter: Table name from where to delete rows
+        """
         metadata = MetaData()
-        sql_table = Table(table_filter, 
+        sql_table = Table(self._TABLE_NAME, 
         metadata, autoload=True, autoload_with=engine)
-        connection = engine.connect()
+        connection = self.engine.connect()
         u = delete(sql_table)
         result = connection.execute(u)
         return result
+
+    # --------------------------------------------------
+    def to_sql(self, sql_path:str, replace:bool = False):
+        """From DataFrame to sql DataBase"""
+        self.engine = self._SQL_MODEL(sql_path).engine        
+        if replace:
+            self.delete_all_rows()            
+        else:
+            self.delete_rows_with_df_col()
+        
+        self.df.to_sql(
+            name = self._TABLE_NAME,
+            con = self.engine,
+            if_exists = 'append',
+            index=False
+        )
+        self.engine.dispose()
+
+    # --------------------------------------------------
+    def from_sql(self, sql_path:str) -> pd.DataFrame:
+        """From sql DataBase to sql DataFrame"""
+        engine = create_engine(f'sqlite:///{sql_path}')
+        self.df = pd.read_sql_table(
+            table_name = self._TABLE_NAME,
+            con = engine,
+            index_col = self._INDEX_COL
+        )
+        engine.dispose()
+        return self.df
+
+    # --------------------------------------------------
+    def test_sql(self, sql_path:str):
+        """Create DB for testing purposes"""
+        engine = create_engine(f'sqlite:///{sql_path}')
+        self.df.to_sql(
+            name = 'test',
+            con = engine,
+            if_exists='replace',
+            index=False
+        )
+        engine.dispose()
+        print('Sqlite test done')
