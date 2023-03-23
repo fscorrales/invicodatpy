b97d4639b4928645265b78607c75ed7dc3c325e8
Fernando Corrales <fscorrales@gmail.com>
2023-03-23 08:06:03 -0300

    feat: add download method to rec_rci02.py

--- src/invicodatpy/siif/comprobantes_rec_rci02.py
+++ src/invicodatpy/siif/comprobantes_rec_rci02.py
@@ -5,23 +5,133 @@ Purpose: Read, process and write SIIF's rci02 report
 """
 
 import argparse
+import datetime as dt
 import inspect
+import json
 import os
+import time
+from dataclasses import dataclass, field
 
 import pandas as pd
 from datar import base, dplyr, f, tidyr
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import Select
 
 from ..models.siif_model import SIIFModel
 from ..utils.rpw_utils import RPWUtils
+from .connect_siif import ConnectSIIF
 
 
+@dataclass
 class ComprobantesRecRci02(RPWUtils):
-    """Read, process and write SIIF's rci02 report"""
-    _REPORT_TITLE = 'RESUMEN DIARIO DE COMPROBANTES DE RECURSOS'
-    _TABLE_NAME = 'comprobantes_rec_rci02'
-    _INDEX_COL = 'id'
-    _FILTER_COL = ['mes']
-    _SQL_MODEL = SIIFModel
+    """
+    Read, process and write SIIF's rci02 report
+    :param siif_connection must be initialized first in order to download from SIIF
+    """
+    _REPORT_TITLE:str = field(
+        init=False, repr=False, 
+        default='RESUMEN DIARIO DE COMPROBANTES DE RECURSOS'
+    )
+    _TABLE_NAME:str = field(
+        init=False, repr=False, 
+        default='comprobantes_rec_rci02'
+    )
+    _INDEX_COL:str = field(
+        init=False, repr=False, default='id'
+    )
+    _FILTER_COL:str = field(
+        init=False, repr=False, default='mes'
+    )
+    _SQL_MODEL:SIIFModel = field(
+        init=False, repr=False, default=SIIFModel
+    )
+    siif:ConnectSIIF = field(
+        init=True, repr=False, default=None
+    )
+
+    # --------------------------------------------------
+    def download_report(
+        self, dir_path:str, ejercicios:list = str(dt.datetime.now().year)
+    ):
+        try:
+            # Path de salida
+            params = {
+            'behavior': 'allow',
+            'downloadPath': dir_path
+            }
+            self.siif.driver.execute_cdp_cmd('Page.setDownloadBehavior', params)
+
+            # Seleccionar módulo Gastos
+            cmb_modulos = Select(
+                self.siif.driver.find_element(By.XPATH, "//select[@id='pt1:socModulo::content']")
+            )
+            cmb_modulos.select_by_visible_text('SUB - SISTEMA DE CONTROL de RECURSOS')
+            time.sleep(1)
+
+            # Select rci02 report
+            input_filter = self.siif.driver.find_element(
+                By.XPATH, "//input[@id='_afrFilterpt1_afr_pc1_afr_tableReportes_afr_c1::content']"
+            )
+            input_filter.clear()
+            input_filter.send_keys('33', Keys.ENTER)
+            btn_siguiente = self.siif.driver.find_element(By.XPATH, "//div[@id='pt1:pc1:btnSiguiente']")
+            btn_siguiente.click()
+
+            # Llenado de inputs
+            self.siif.wait.until(EC.presence_of_element_located(
+                (By.XPATH, "//input[@id='pt1:txtAnioEjercicio::content']")
+            ))
+            input_ejercicio = self.siif.driver.find_element(
+                    By.XPATH, "//input[@id='pt1:txtAnioEjercicio::content']"
+                )
+            input_fecha_desde = self.siif.driver.find_element(
+                    By.XPATH, "//input[@id='pt1:idFechaDesde::content']"
+                )
+            input_fecha_hasta = self.siif.driver.find_element(
+                    By.XPATH, "//input[@id='pt1:idFechaHasta::content']"
+                )
+            btn_get_reporte = self.siif.driver.find_element(By.XPATH, "//div[@id='pt1:btnEjecutarReporte']")
+            btn_xls = self.siif.driver.find_element(By.XPATH, "//input[@id='pt1:rbtnXLS::content']")
+            btn_xls.click()
+            if not isinstance(ejercicios, list):
+                ejercicios = [ejercicios]
+            for ejercicio in ejercicios:
+                int_ejercicio = int(ejercicio)
+                if int_ejercicio > 2010 and int_ejercicio <= dt.datetime.now().year:
+                    # Ejercicio
+                    input_ejercicio.clear()
+                    input_ejercicio.send_keys(ejercicio)
+                    # Fecha Desde
+                    input_fecha_desde.clear()
+                    fecha_desde = dt.datetime.strftime(
+                        dt.date(year=int_ejercicio, month=1, day=1),
+                        '%d/%m/%Y'
+                    )
+                    input_fecha_desde.send_keys(fecha_desde)
+                    # Fecha Hasta
+                    input_fecha_hasta.clear()
+                    fecha_hasta = dt.datetime(year=(int_ejercicio+1), month=12, day=31)
+                    fecha_hasta = min(fecha_hasta, dt.datetime.now())
+                    fecha_hasta = dt.datetime.strftime(fecha_hasta, '%d/%m/%Y'
+                    )
+                    input_fecha_hasta.send_keys(fecha_hasta)
+                    btn_get_reporte.click()
+                    self.siif.rename_report(dir_path, 'rci02.xls', ejercicio + '-rci02.xls')
+                    self.siif.wait.until(EC.number_of_windows_to_be(3))
+                    self.siif.driver.switch_to.window(self.siif.driver.window_handles[2])
+                    self.siif.driver.close()
+                    self.siif.driver.switch_to.window(self.siif.driver.window_handles[1])
+            time.sleep(1)
+            btn_volver = self.siif.driver.find_element(By.XPATH, "//div[@id='pt1:btnVolver']")
+            btn_volver.click()
+            time.sleep(1)
+
+        except Exception as e:
+            print(f"Ocurrió un error: {e}, {type(e)}")
+            self.siif.disconnect()
+
 
     # --------------------------------------------------
     def from_external_report(self, xls_path:str) -> pd.DataFrame:
@@ -87,6 +197,34 @@ def get_args():
         type=str,
         help = "SIIF' rci02.xls report. Must be in the same folder")
 
+    parser.add_argument(
+        '-d', '--download', 
+        metavar = "download",
+        default=True,
+        type=bool,
+        help = "Download data")
+
+    parser.add_argument(
+        '-u', '--username', 
+        metavar = 'Username',
+        default = '',
+        type=str,
+        help = "Username to log in SIIF")
+
+    parser.add_argument(
+        '-p', '--password', 
+        metavar = 'Password',
+        default = '',
+        type=str,
+        help = "Password to log in SIIF")
+
+    parser.add_argument(
+        '-e', '--ejercicio', 
+        metavar = 'Ejercicio',
+        default = '2023',
+        type=str,
+        help = "Ejercicio to download from SIIF")
+
     return parser.parse_args()
 
 # --------------------------------------------------
@@ -97,8 +235,33 @@ def main():
         os.path.abspath(
             inspect.getfile(
                 inspect.currentframe())))
-    siif_rci02 = ComprobantesRecRci02()
-    siif_rci02.from_external_report(dir_path + '/' + args.file)
+
+    if args.download:
+        json_path = dir_path + '/siif_credentials.json'
+        if args.username != '' and args.password != '':
+            siif_connection = ConnectSIIF(args.username, args.password)
+        else:
+            if os.path.isfile(json_path):
+                with open(json_path) as json_file:
+                    data_json = json.load(json_file)
+                    siif_connection = ConnectSIIF(
+                        data_json['username'], data_json['password']
+                    )
+                json_file.close()
+        siif_rci02 = ComprobantesRecRci02(siif = siif_connection)
+        siif_rci02.download_report(
+            dir_path, ejercicios=args.ejercicio
+        )
+        siif_connection.disconnect()
+    else:
+        siif_rci02 = ComprobantesRecRci02()
+
+    if args.file != '':
+        filename = args.file
+    else:
+        filename = args.ejercicio + '-rci02.xls'
+
+    siif_rci02.from_external_report(dir_path + '/' + filename)
     # siif_rci02.test_sql(dir_path + '/test.sqlite')
     siif_rci02.to_sql(dir_path + '/siif.sqlite')
     siif_rci02.print_tidyverse()
