93bc1d0a7bab313294b65ffd298d8388cdeff656
Fernando Corrales <fscorrales@gmail.com>
2024-08-09 08:11:08 -0300

    feat

--- src/invicodatpy/siif/connect_siif.py
+++ src/invicodatpy/siif/connect_siif.py
@@ -20,6 +20,7 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.keys import Keys
 from webdriver_manager.chrome import ChromeDriverManager
 
 
@@ -73,9 +74,12 @@ class ConnectSIIF():
             input_username, input_password = self.driver.find_elements(By.XPATH, "//input[starts-with(@id,'pt')]")
             input_username.send_keys(self.username)
             input_password.send_keys(self.password) 
+            input_password.send_keys(Keys.ENTER)  # Presionar la tecla Enter
             # btn_connect = self.driver.find_element(By.XPATH, "//div[@id='pt1:t1::oc']")
-            btn_connect = self.driver.find_element(By.XPATH, "//button[@id='pt1:cb1']")
-            btn_connect.click()
+            # btn_connect = self.driver.find_element(By.XPATH, "//button[@id='pt1:cb1']")
+            # btn_connect.click()
+            time.sleep(1)
+            input_password.send_keys(Keys.ENTER)  # Presionar la tecla Enter
             time.sleep(1)
         except NoSuchElementException:
             print(f"No se encontro el elemento: {NoSuchElementException}")
