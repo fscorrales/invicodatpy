a0b14e1b39b4d9c15ada4e3f8a6093672a714835
Fernando Corrales <fscorrales@gmail.com>
2024-10-29 11:38:56 -0300

    minor fix in rpa03g

--- src/invicodatpy/siif/comprobantes_gtos_gpo_part_gto_rpa03g.py
+++ src/invicodatpy/siif/comprobantes_gtos_gpo_part_gto_rpa03g.py
@@ -16,10 +16,6 @@ from dataclasses import dataclass, field
 
 import numpy as np
 import pandas as pd
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import Select
 
 from ..models.siif_model import SIIFModel
 from .connect_siif import ConnectSIIF
